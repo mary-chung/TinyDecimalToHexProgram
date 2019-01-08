1bb11d422c4075e120773620c8e271995055812c
maryc <marychung@live.com>
2019-01-08 13:17:25 -0800

    Just realised scratch files don't get committed. Copied them to src.

---
+++ .idea/src/IllegalInput.java
@@ -0,0 +1,3 @@
+class IllegalInput extends Exception {
+
+}
---
+++ .idea/src/Main.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+class Main {
+    public static void main(String[] args) {
+        Scanner reader = new Scanner(System.in);
+        System.out.println("Enter \"break\" to exit");
+        System.out.println("Enter a number: ");
+        String input = reader.next();
+        while (!input.equals("break")) {
+            int i = Integer.parseInt(input);
+            System.out.printf("0x%x\n", i);
+            System.out.println("Enter new number: ");
+            input = reader.next();
+        }
+    }
+}
